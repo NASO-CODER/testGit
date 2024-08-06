47c849e920a725f38c96b55a4b52f0f75d4c015a
asus <asus@DESKTOP-T3DJ34V>
2024-08-06 12:13:03 +0900

    다섯번째 리퀘스트

--- TestGitProject/src/main/java/test/TestMain.java
+++ TestGitProject/src/main/java/test/TestMain.java
@@ -4,6 +4,6 @@ public class TestMain {
 public static void main(String[] args) {
 	System.out.println("안녕하세요");
 	System.out.println("ㅇㅇㅇㅇㅇㅇㅇ");
-	System.out.println(dddvcx);
+	System.out.println("dddvcx");
 }
 }
